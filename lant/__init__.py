801c35f015af386dfe010c47487c049fa7f1dc0a
Jacan Chaplais <jacan.chaplais@gmail.com>
2022-08-29 16:44:01 +0100

    implemented a video output

--- lant/__init__.py
+++ lant/__init__.py
@@ -1,20 +1,37 @@
 from dataclasses import dataclass
 from enum import Enum
-from typing import List, BinaryIO, Union
+from typing import Tuple
 from pathlib import Path
+from contextlib import contextmanager
 
-from PIL import Image
 import numpy as np
+import cv2
 
 
-def array_to_img(array: np.ndarray, width: int, height: int) -> Image.Image:
-    return Image.fromarray(array).convert("P").resize((width, height))
-                
+def create_grid(dims: Tuple[int, int], init: str) -> np.ndarray:
+    if init == "noise":
+        rng = np.random.default_rng()
+        grid = rng.integers(0, 2, size=dims, dtype="<?")
+    else:
+        grid = np.zeros(dims, dtype="<?")
+        if init == "white":
+            grid[...] = True
+    return grid
 
-def make_gif(path: Union[Path, BinaryIO], images: List[Image.Image]) -> None:
-    images[0].save(
-            path, save_all=True, append_images=images[1:], optimize=False,
-            duration=40, loop=0)
+
+def array_as_frame(array: np.ndarray, scale: int) -> np.ndarray:
+    frame = array.astype("uint8") * 255
+    return np.repeat(np.repeat(frame, scale, axis=0), scale, axis=1)
+
+
+@contextmanager
+def video_writer(path: Path, codec: str, rate: int, res: Tuple[int, int]):
+    fourcc = cv2.VideoWriter_fourcc(*codec)
+    video_file = cv2.VideoWriter(str(path), fourcc, rate, res, False)
+    try:
+        yield video_file
+    finally:
+        video_file.release()
 
 
 class Direction(Enum):
@@ -31,7 +48,9 @@ class Ant:
     y: int
     _angle: Direction
 
-    def move(self, cell: bool) -> None:
+    def move(self, grid: np.ndarray) -> np.ndarray:
+        cell = grid[self.y, self.x]
+        grid[self.y, self.x] = ~cell
         # direction change
         if cell == True:
             self.angle = self.angle.value + 90
@@ -46,6 +65,8 @@ class Ant:
             self.x -= 1
         elif self.angle == Direction.UP:
             self.y -= 1
+        # release the updated grid
+        return grid
 
     @property
     def angle(self) -> Direction:
@@ -54,3 +75,8 @@ class Ant:
     @angle.setter
     def angle(self, val: int) -> None:
         self._angle = Direction(val % 360)
+
+    def bounded(self, bounds: Tuple[int, int]) -> bool:
+        by, bx = bounds
+        return ((self.x >= 0) and (self.x < bx)
+                and (self.y >= 0) and (self.y < by))
--- lant/__main__.py
+++ lant/__main__.py
@@ -2,48 +2,40 @@
 # -*- coding: utf-8 -*-
 import sys
 from pathlib import Path
-from typing import Tuple, BinaryIO
+from typing import Tuple
 
 import click
 import numpy as np
 
-from . import Ant, array_to_img, make_gif, Direction
+from . import Ant, Direction, video_writer, array_as_frame, create_grid
 
 
 @click.command()
-@click.argument("max-steps", type=click.IntRange(10, 10_000, clamp=True))
-@click.argument("output", type=click.File("wb"))
+@click.argument("max-steps", type=click.IntRange(1, None))
+@click.argument("output", type=click.Path(readable=False, writable=True,
+                                          path_type=Path))
 @click.option("--init", default="white", show_default=True,
               type=click.Choice(["black", "white", "noise"],
                                 case_sensitive=False))
-@click.option("--res", nargs=2, default=(1000, 1000),
-              type=click.IntRange(100, None, clamp=True), show_default=True)
 @click.option("--grid-size", nargs=2, default=(250, 250),
               type=click.IntRange(10, None, clamp=True), show_default=True)
-def main(max_steps: int, output: BinaryIO, init: str, res: Tuple[int, int],
-         grid_size: Tuple[int, int]) -> None:
-    bound_x, bound_y = grid_size
-    grid = np.zeros((bound_x, bound_y), dtype="<?")
-    if init == "white":
-        grid[...] = True
-    elif init == "noise":
-        rng = np.random.default_rng()
-        grid = rng.integers(0, 2, size=(bound_x, bound_y), dtype="<?")
+@click.option("--scale", type=click.IntRange(None, 10), show_default=True,
+              default=4, help="Factor by which to rescale the grid for video.")
+@click.option("--rate", type=click.INT, default=300, show_default=True)
+def main(max_steps: int, output: Path, init: str,
+         grid_size: Tuple[int, int], scale: int, rate: int) -> None:
+    if output.exists() and not click.confirm(f"{output} exists! Overwrite?"):
+        raise click.Abort()
+    bound_y, bound_x = grid_size
+    res = tuple((x * scale for x in grid_size))
+    grid = create_grid(grid_size, init)
     ant = Ant(bound_x // 2, bound_y // 2, Direction.RIGHT)
-    images = []
-    
-    count = 0
-    while ((ant.x < bound_x) and (ant.x > 0)
-           and (ant.y < bound_y) and (ant.y > 0)
-           and (count <= max_steps)):
-        x, y = ant.x, ant.y
-        cell_val = grid[y, x]
-        ant.move(cell_val)
-        grid[y, x] = ~cell_val
-        images.append(array_to_img(grid, *res))
-        count = count + 1
-
-    make_gif(output, images)
+    with video_writer(output, "mp4v", rate, res) as video_file:
+        for _ in range(max_steps):
+            if not ant.bounded(grid_size):
+                break
+            grid = ant.move(grid)
+            video_file.write(array_as_frame(grid, scale))
 
 
 if __name__ == "__main__":
